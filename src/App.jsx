ad951f6e112f440438a57170fa7fde81cdc3b8d9
mahade <mahade.adib45@gmail.com>
2024-03-13 16:53:02 +0600

    added fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './assets/components/Blogs/Blogs'
+import Header from './assets/components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl bg-slate-500'>Knowledge Cafe</h1>
+    <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
